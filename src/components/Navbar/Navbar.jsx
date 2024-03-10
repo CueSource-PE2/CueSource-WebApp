b6cf5a8389a54bc8d35798db6427cfc458506dc7
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-03-11 00:45:41 +0530

    navlink added to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     return (
         <div className=' flex justify-between items-center'>
             <div className='flex items-center md:gap-12 sm:gap-8'>
             <img className='hover:scale-90 cursor-pointer scale-75 transition-all' src="/images/menu-icon.png" alt="menu" />
-            <p className="text-white md:text-3xl sm:text-xl font-bold font-['Inter] cursor-pointer hover:border-b-2 pb-1 transition-all duration-100 ease-in-out">CueSource</p>
+            <NavLink to='https://cue-source-website.vercel.app/'><p className="text-white md:text-3xl sm:text-xl font-bold font-['Inter] cursor-pointer hover:border-b-2 pb-1 transition-all duration-100 ease-in-out">CueSource</p></NavLink>
             </div>
             <div className=' flex items-center justify-center md:gap-10 sm:gap-5'>
             <div className='flex justify-between items-center md:gap-5 sm:gap-1'>
